75841e94ec27b8a5d9a0a3e9ae5404e2677d53dc
Lissa Hyacinth <lissa.hyacinth@forward3d.com>
2020-04-14 13:56:03 +0100

    Add in comment flow for PRs. Update workflow for writing to PRs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,39 @@
 #! /bin/bash -l
 set -uo pipefail
-pyre --source-directory . check
+
+PYRE_ARGS=$1
+GITHUB_TOKEN=$2
+
+post_pr_comment() {
+  local msg=$1
+  payload=$(echo '{}' | jq --arg body "${msg}" '.body = $body')
+  request_url=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+  curl -s -S \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --data "${payload}" \
+    "${request_url}" > /dev/null
+}
+
+main() {
+  pyre --source-directory . check > /tmp/PyreOutput.txt
+  comment_title="Pyre Output"
+  comment_body=""
+
+  comment_body="${comment_body}
+
+<details><summary><code>${output}</code></summary>
+\'\'\'\n
+$(cat /tmp/PyreOutput.txt)
+\`\`\`
+
+</details>
+"
+  comment_msg="## ${comment_title}
+${comment_body}
+"
+
+  post_pr_comment "${comment_msg}"
+}
+
+main "$@"
